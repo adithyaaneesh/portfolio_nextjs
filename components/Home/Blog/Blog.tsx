aadadbddcbe4e5d1b906a8562458d3b2f25517f1
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-12-13 15:28:32 +0530

    add annimation to blog section

--- components/Home/Blog/Blog.tsx
+++ components/Home/Blog/Blog.tsx
@@ -6,19 +6,19 @@ const Blog = () => {
     <div className='pt-16 pb-16'>
       <h2 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>My latest <span className='text-cyan-300'>Blogs</span></h2>
       <div className='w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-3 xl:gap-4 gap-10 items-center mt-16'>
-        <div>
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center" data-aos-delay="0">
             <BlogCard 
                 image="/images/b1.jpg" 
                 title="Learn how to build an amazing portfolio website using next js" 
             />
         </div>
-        <div>
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center" data-aos-delay="100">
             <BlogCard 
                 image="/images/b2.jpg" 
                 title="Learn how to build an amazing Blog website using next js" 
             />
         </div>
-        <div>
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center" data-aos-delay="200">
             <BlogCard 
                 image="/images/b3.jpg" 
                 title="Learn how to build an amazing Social Media website using next js" 
