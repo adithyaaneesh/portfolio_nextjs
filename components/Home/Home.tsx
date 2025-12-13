bf59e6c47f19c40d682d3a28f6fd416221a21092
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-12-13 16:06:45 +0530

    fix issue of home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,44 +1,39 @@
 'use client';
-import React, { useEffect } from 'react'
-import Hero from './Hero/Hero'
-import Services from './Services/Services'
-import Resume from './Resume/Resume'
-import Projects from './Projects/Projects'
-import Skills from './Skills/Skills'
-import ClientReview from './ClientReview/ClientReview'
-import Blog from './Blog/Blog'
-import Contact from './Contact/Contact'
 
-import AOS from 'aos';
+import { useEffect } from 'react';
+import Hero from './Hero/Hero';
+import Services from './Services/Services';
+import Resume from './Resume/Resume';
+import Projects from './Projects/Projects';
+import Skills from './Skills/Skills';
+import ClientReview from './ClientReview/ClientReview';
+import Blog from './Blog/Blog';
+import Contact from './Contact/Contact';
+
 import 'aos/dist/aos.css';
 
-const Home = () => {
-
-  useEffect(()=>{
-    const initAOS = async()=>{
-      await import('aos');
-      AOS.init({
-        duration:1000,
-        easing:'ease',
-        once:true,
-        anchorPlacement:'top-bottom',
+export default function Home() {
+  useEffect(() => {
+    import('aos').then((AOS) => {
+      AOS.default.init({
+        duration: 1000,
+        easing: 'ease',
+        once: true,
+        anchorPlacement: 'top-bottom',
       });
-    };
+    });
+  }, []);
 
-    initAOS();
-  },[]);
   return (
-    <div className='overflow-hidden'>
-      <Hero/>
-      <Services/>
-      <Resume/>
-      <Projects/>
-      <Skills/>
-      <ClientReview/>
-      <Blog/>
-      <Contact/>
+    <div className="overflow-hidden">
+      <Hero />
+      <Services />
+      <Resume />
+      <Projects />
+      <Skills />
+      <ClientReview />
+      <Blog />
+      <Contact />
     </div>
-  )
+  );
 }
-
-export default Home
