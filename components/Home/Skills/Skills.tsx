0183ab903668a1d512aeebbf61619cec40471197
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-01-30 14:33:09 +0530

    minor change in transition duration

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -84,7 +84,7 @@ const Skills = () => {
         data-aos-easing="ease-in-out"
         >
             {skills.map((skill,i)=>{
-                return <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+                return <Tilt key={skill.name} scale={1.5} transitionSpeed={600}>
                     <div data-aos="flip-right" data-aos-anchor-placement="top-center" data-aos-delay={i * 100} className='bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center 
                     justify-center shadow-lg transition hover:scale-105'>
                         <div className='text-5xl mb-4 text-gray-300'>{skill.icon}</div>
